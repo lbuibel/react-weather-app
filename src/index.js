e682b508713bd5d79863dbafcb803210654fcad6
Logan Uibel <lbuibel@gmail.com>
2021-03-18 18:04:34 -0600

    overahauled styling and formatting

--- src/index.js
+++ src/index.js
@@ -26,8 +26,8 @@ function WeatherApp() {
     <div className="App">
      <div className="weather-container">
 
-       <div>
-        <p>Select City</p>
+       <div className="city-select">
+        <h3>Select City</h3>
           <select onChange={(city) => changeCity(city)}>
             <option value='New York'>New York</option>
             <option value='Los Angeles'>Los Angeles</option>
@@ -36,64 +36,111 @@ function WeatherApp() {
         </div>
 
        <div className="current-weather">
-         <h1>City Name: {city[0].name}</h1>
+         <h1>{city[0].name}</h1>
+         <p className="current-temp">{city[0].conditions.feels_like}<span>&#176;</span></p>
          <p>Current Conditions: {city[0].conditions.description}</p>
-         <p>Feels Like: {city[0].conditions.feels_like}</p>
          <p>Wind Speed: {city[0].wind.speed} mph</p>
          <p>Humidity: {city[0].conditions.humidity} mph</p>
        </div>
 
         <div className="forcast">
+         {/* Monday  */}
          <div className="daily-weather">
-           <h3>Monday</h3>
-           <p>High: {city[0].forcast[0].high}</p>
-           <p>Low: {city[0].forcast[0].low}</p>
+           <div className="row">
+            <h3>Monday</h3>
+            <div>
+            <p>
+              <span className="high">{city[0].forcast[0].high}<span>&#176;</span></span> / 
+              <span className="low"> {city[0].forcast[0].low}<span>&#176;</span></span>
+            </p>
+            </div>
+           </div>
            <p>Conditions: {city[0].forcast[0].conditions}</p>
          </div>
 
+         {/* Tuesday  */}
          <div className="daily-weather">
-           <h3>Tuesday</h3>
-           <p>High: {city[0].forcast[1].high}</p>
-           <p>Low: {city[0].forcast[1].low} </p>
+           <div className="row">
+            <h3>Tuesday</h3>
+            <div>
+            <p>
+              <span className="high">{city[0].forcast[1].high}<span>&#176;</span></span> / 
+              <span className="low"> {city[0].forcast[1].low}<span>&#176;</span></span>
+            </p>
+            </div>
+           </div>
            <p>Conditions: {city[0].forcast[1].conditions}</p>
          </div>
 
+         {/* Wednesday  */}
          <div className="daily-weather">
-           <h3>Wednesday</h3>
-           <p>High: {city[0].forcast[2].high}</p>
-           <p>Low: {city[0].forcast[2].low} </p>
+           <div className="row">
+            <h3>Wednesday</h3>
+            <div>
+            <p>
+              <span className="high">{city[0].forcast[2].high}<span>&#176;</span></span> / 
+              <span className="low"> {city[0].forcast[2].low}<span>&#176;</span></span>
+            </p>
+            </div>
+           </div>
            <p>Conditions: {city[0].forcast[2].conditions}</p>
          </div>
 
+         {/* Thursday  */}
          <div className="daily-weather">
-           <h3>Thursday</h3>
-           <p>High: {city[0].forcast[3].high}</p>
-           <p>Low: {city[0].forcast[3].low} </p>
+           <div className="row">
+            <h3>Thursday</h3>
+            <div>
+            <p>
+              <span className="high">{city[0].forcast[3].high}<span>&#176;</span></span> / 
+              <span className="low"> {city[0].forcast[3].low}<span>&#176;</span></span>
+            </p>
+            </div>
+           </div>
            <p>Conditions: {city[0].forcast[3].conditions}</p>
-
          </div>
 
+         {/* Friday  */}
          <div className="daily-weather">
-           <h3>Friday</h3>
-           <p>High: {city[0].forcast[4].high}</p>
-           <p>Low: {city[0].forcast[4].low} </p>
+           <div className="row">
+            <h3>Friday</h3>
+            <div>
+            <p>
+              <span className="high">{city[0].forcast[4].high}<span>&#176;</span></span> / 
+              <span className="low"> {city[0].forcast[4].low}<span>&#176;</span></span>
+            </p>
+            </div>
+           </div>
            <p>Conditions: {city[0].forcast[4].conditions}</p>
          </div>
 
+         {/* Saturday  */}
          <div className="daily-weather">
-           <h3>Saturday</h3>
-           <p>High: {city[0].forcast[5].high}</p>
-           <p>Low: {city[0].forcast[5].low} </p>
-           <p>Conditions: {city[0].forcast[5].conditions}</p>
+           <div className="row">
+            <h3>Saturday</h3>
+            <div>
+            <p>
+              <span className="high">{city[0].forcast[5].high}<span>&#176;</span></span> / 
+              <span className="low"> {city[0].forcast[5].low}<span>&#176;</span></span>
+            </p>
+            </div>
+           </div>
+           <p>Conditions: {city[0].forcast[4].conditions}</p>
          </div>
 
+         {/* Sunday  */}
          <div className="daily-weather">
-           <h3>Sunday</h3>
-           <p>High: {city[0].forcast[6].high}</p>
-           <p>Low: {city[0].forcast[6].low} </p>
+           <div className="row">
+            <h3>Sunday</h3>
+            <div>
+            <p>
+              <span className="high">{city[0].forcast[6].high}<span>&#176;</span></span> / 
+              <span className="low"> {city[0].forcast[6].low}<span>&#176;</span></span>
+            </p>
+            </div>
+           </div>
            <p>Conditions: {city[0].forcast[6].conditions}</p>
          </div>
-
        </div>
      </div>
 
@@ -110,20 +157,20 @@ function WeatherApp() {
        </select>
 
        <div className = "hour-report">
-        <p>Day: {currentDay}</p>
-        <p>8 AM: {city[0][currentDay].eightAM}</p>
-        <p>9 AM: {city[0][currentDay].nineAM}</p>
-        <p>10 AM: {city[0][currentDay].tenAM}</p>
-        <p>11 AM: {city[0][currentDay].elevenAM}</p>
-        <p>12 AM: {city[0][currentDay].twelvePM}</p>
-        <p>1 PM: {city[0][currentDay].onePM}</p>
-        <p>2 PM: {city[0][currentDay].twoPM}</p>
-        <p>3 AM: {city[0][currentDay].threePM}</p>
-        <p>4 AM: {city[0][currentDay].fourPM}</p>
-        <p>5 AM: {city[0][currentDay].fivePM}</p>
-        <p>6 AM: {city[0][currentDay].sixPM}</p>
-        <p>7 AM: {city[0][currentDay].sevenPM}</p>
-        <p>8 AM: {city[0][currentDay].eightPM}</p>
+        <h5>Day: {currentDay}</h5>
+        <p>8am: {city[0][currentDay].eightAM}<span>&#176;</span></p>
+        <p>9am: {city[0][currentDay].nineAM}<span>&#176;</span></p>
+        <p>10am: {city[0][currentDay].tenAM}<span>&#176;</span></p>
+        <p>11am: {city[0][currentDay].elevenAM}<span>&#176;</span></p>
+        <p>12pm: {city[0][currentDay].twelvePM}<span>&#176;</span></p>
+        <p>1pm: {city[0][currentDay].onePM}<span>&#176;</span></p>
+        <p>2pm: {city[0][currentDay].twoPM}<span>&#176;</span></p>
+        <p>3pm: {city[0][currentDay].threePM}<span>&#176;</span></p>
+        <p>4pm: {city[0][currentDay].fourPM}<span>&#176;</span></p>
+        <p>5pm: {city[0][currentDay].fivePM}<span>&#176;</span></p>
+        <p>6pm: {city[0][currentDay].sixPM}<span>&#176;</span></p>
+        <p>7pm: {city[0][currentDay].sevenPM}<span>&#176;</span></p>
+        <p>8pm: {city[0][currentDay].eightPM}<span>&#176;</span></p>
 
        </div>
      </div>
